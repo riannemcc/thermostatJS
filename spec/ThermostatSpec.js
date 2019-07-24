323ebcdc1931869d565424c82d3549346c058558
Freddie <f.varnals@gmail.com>
2019-07-24 16:13:30 +0100

    Power saving mode functionality added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -18,11 +18,41 @@ describe('Thermostat', function(){
   });
 
   it('prevents temperature from being decreased to less than 10', function(){
-    var step;
-    for(let step = 0; step < 10; step++) {
+    // var step;
+    // for(let step = 0; step < 10; step++) {
+    //   thermostat.decrease();
+    // }
+    while (thermostat.getCurrentTemp() > 10 ) {
       thermostat.decrease();
     }
     expect(function(){ thermostat.decrease();}).toThrowError('cannot decrease the temperature below 10');
   })
 
+  describe('Power Saving Mode', function() {
+    it('Sets max temperature to 25 when ON', function(){
+      thermostat.powerSavingModeOn();
+      expect(thermostat._maxTemperature).toEqual(25);
+    });
+
+    it('Sets max temperature to 32 when OFF', function(){
+      thermostat.powerSavingModeOff();
+      expect(thermostat._maxTemperature).toEqual(32);
+    });
+
+    it('Prevents temperature from being increased above 25 when ON', function() {
+      thermostat.powerSavingModeOn();
+      while (thermostat.getCurrentTemp() < 25 ) {
+        thermostat.increase();
+      }
+      expect(function(){ thermostat.increase();}).toThrowError('cannot increase the temperature above 25');
+    });
+
+    it('Prevents temperature from being increased above 32 when OFF', function() {
+      thermostat.powerSavingModeOff();
+      while (thermostat.getCurrentTemp() < 32 ) {
+        thermostat.increase();
+      }
+      expect(function(){ thermostat.increase();}).toThrowError('cannot increase the temperature above 32');
+    });
+  })
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,16 @@
 function Thermostat(){
   this._temperature = 20;
+  this._maxTemperature = 25;
+  this._powerSavingMode = 'ON';
 };
 Thermostat.prototype.getCurrentTemp = function(){
   return this._temperature;
 };
 
 Thermostat.prototype.increase = function(){
+  if(this.getCurrentTemp() === this._maxTemperature){
+    throw new Error(`cannot increase the temperature above ${this._maxTemperature}`)
+  }
   this._temperature += 1;
 };
 
@@ -14,4 +19,15 @@ Thermostat.prototype.decrease = function(){
     throw new Error('cannot decrease the temperature below 10');
   }
     this._temperature -= 1;
+};
+
+Thermostat.prototype.powerSavingModeOn = function(){
+  this._powerSavingMode = 'ON';
+  this._maxTemperature = 25;
+
+};
+
+Thermostat.prototype.powerSavingModeOff = function(){
+  this._powerSavingMode = 'OFF';
+  this._maxTemperature = 32;
 };
